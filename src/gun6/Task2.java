a47d6b28b309f6bec5386460c26285c564858f2c
smlblc <samilbalci.us@gmail.com>
2020-09-05 17:23:50 -0400

    Q: Task2

---
+++ src/gun6/Task2.java
@@ -0,0 +1,9 @@
+package gun6;
+
+public class Task2 {
+    // create a string with value "admin@techno.study"
+    // search for the "study" and return its index;
+    public static void main(String[] args) {
+        
+    }
+}
