e095957ff82773b743814e43c5bbe321628da683
smlblc <samilbalci.us@gmail.com>
2020-09-05 17:27:53 -0400

    Q: Task3

---
+++ src/gun6/Task3.java
@@ -0,0 +1,15 @@
+package gun6;
+
+public class Task3 {
+    //create two variables as first name , and last name
+    // print initials of your name
+    // input:
+    // a = Michael
+    // b = Jackson
+
+    // output:
+    // M.J.
+    public static void main(String[] args) {
+        
+    }
+}
