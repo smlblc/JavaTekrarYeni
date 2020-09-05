3d33eadbb3f538efb656cdff36524d67b2178725
smlblc <samilbalci.us@gmail.com>
2020-09-05 15:52:49 -0400

    Scanner class

--- src/gun5/ScannerClass.java
+++ src/gun5/ScannerClass.java
@@ -8,6 +8,34 @@ public class ScannerClass {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Biletiniz cift yonlu olsun mu?");
+//        System.out.println("Biletiniz cift yonlu olsun mu? (True/False)");
+//        boolean biletCiftYonluMu = scanner.nextBoolean(); //Ctrl+C ile secip Ctrl+Alt+V
+//        System.out.println("Biletinit cift Yonlu: " + biletCiftYonluMu);
+//
+//        System.out.println("Biletiniz ucreti icin max rakam? Double olarak giriniz");
+//        double biletUcreti = scanner.nextDouble(); //Ctrl+C ile secip Ctrl+Alt+V
+//        System.out.println("Biletiniz icin max rakam : " + biletUcreti);
+//
+//        System.out.println("Biletiniz ucreti icin max rakam? Float olarak giriniz");
+//        float biletUcretiFloat = scanner.nextFloat(); //Ctrl+C ile secip Ctrl+Alt+V
+//        System.out.println("Biletiniz icin max rakam : " + biletUcretiFloat);
+//
+//        //short  --> nextShort()
+//        System.out.println( "Toplam miktar bilet icin?" );
+//        short biletToplamShort = scanner.nextShort();
+//
+//        System.out.println( "Toplam miktar bilet icin: " + biletToplamShort );
+
+
+        //byte  ->> nextByte()  -128 -- 127
+        System.out.println( "Kac bilet almak istiyorsunuz? " );
+        byte toplamBiletsayisi = scanner.nextByte();
+        System.out.println("Toplam "+toplamBiletsayisi+ " bilet aldiniz.");
+
+        //scanner ile char kullanamiyoruz
+//        System.out.println( "Bir karakter giriniz? " );
+//        char karakter = scanner.nextChar();
+//        System.out.println("girdiginiz "+karakter+ ".");
+
     }
 }
