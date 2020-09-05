0671502f1bf5d2c963b39c75d8c45ce3157878e8
smlblc <samilbalci.us@gmail.com>
2020-09-05 15:19:37 -0400

    Initial commit

---
+++ src/gun5/ScannerClass.java
@@ -0,0 +1,13 @@
+package gun5;
+
+import java.util.Scanner;
+
+public class ScannerClass {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Biletiniz cift yonlu olsun mu?");
+    }
+}
