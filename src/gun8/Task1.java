f1d1844245c0163a7c3b39938d0cc9247b5261e5
smlblc <samilbalci.us@gmail.com>
2020-09-12 16:36:49 -0400

    AO Task2 Soru

--- src/gun8/Task1.java
+++ src/gun8/Task1.java
@@ -1,8 +1,37 @@
 package gun8;
 
+import java.util.Scanner;
+
 public class Task1 {
 
     // Write a Java program that multiplies four integers and  print result
 
     //part 2: use scanner to provide numbers
+
+    public static void main(String[] args) {
+
+        System.out.println("4 sayi carpimi: " + 2*3*4*5);
+
+        int a = 5;
+        int b = 6;
+        int c = 3;
+        int d = 2;
+        System.out.println(a*b*c*d);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("1.Sayı: ");
+        int sayi1 = scanner.nextInt();
+
+        System.out.println("2.Sayı: ");
+        int sayi2 = scanner.nextInt();
+
+        System.out.println("3.Sayı: ");
+        int sayi3 = scanner.nextInt();
+
+        System.out.println("4.Sayı: ");
+        int sayi4 = scanner.nextInt();
+
+        System.out.println("Carpim = " + sayi1*sayi2*sayi3*sayi4);
+    }
 }
---
+++ src/gun8/Task2.java
@@ -0,0 +1,14 @@
+package gun8;
+
+public class Task2 {
+    // Write a Java program that print the average of three integers:
+    // a, b, c (a = 5, b= 3, c= 2)
+    // output:
+    // (a+b+c)/3 = 3.33333333333
+
+    // part 2: provide numbers using scanner
+
+    public static void main(String[] args) {
+        
+    }
+}
