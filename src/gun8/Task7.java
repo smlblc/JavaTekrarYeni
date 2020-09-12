27ff853cc5d02ceeeae2837af6ea9b0cfd74beca
smlblc <samilbalci.us@gmail.com>
2020-09-12 17:29:04 -0400

    AO Task7 Soru-cevap

--- src/gun8/Task7.java
+++ src/gun8/Task7.java
@@ -13,5 +13,12 @@ public class Task7 {
 
     public static void main(String[] args) {
 
+        int x=12345;
+        int birler=x%10;
+        int onlar=(x/10)%10;
+        int yuzler=(x/100)%10;
+        int binler=(x/1000)%10;
+        int onbinler=(x/10000);
+        System.out.println(onbinler+"\n"+binler+"\n"+yuzler+"\n"+onlar+"\n"+birler);
     }
 }
